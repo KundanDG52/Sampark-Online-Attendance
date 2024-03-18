e6b7e753705e0215a512f4d94cf7e941f77f5817
KundanDG52 <gahalotkundan@gmail.com>
2024-03-18 15:46:39 +0530

    new commit

--- common/db.js
+++ common/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+// Connecting Database
 const connectedDb = async () => {
   try {
     const connect = await mongoose.connect("mongodb://0.0.0.0:27017/", {
